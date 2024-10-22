704636786a2fec4c1a6b9693885e671b7b54d374
Kishore-Manubolu <manubolukishore@gmail.com>
2024-10-22 21:47:34 +0530

    dev1 changes

--- Sample.java
+++ Sample.java
@@ -1,4 +1,7 @@
 public class Sample {
+    public Sample(){
+        System.out.println("Sample()");
+    }
     public static void main(String[] args) {
         System.out.println("hello sample");
     }
